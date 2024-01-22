5bf49afc53ec11d8d3308fb681d47d7ff2fd0c6a
akhil-iitkgp <akhilaashiyan17@gmail.com>
2024-01-22 23:02:57 +0530

    first commit

---
+++ middlewares/auth.js
@@ -0,0 +1,87 @@
+
+//
+
+
+// auth, isStudent,isAdmin
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// authontication check kar rha hai bas
+exports.auth = (req,res, next) => {     // synta hai next kyo use hota hai important hai
+    try{
+        //extract JWT token
+        //PENDING : other ways to fetch token
+
+        console.log("cookie" , req.cookies.token);
+        console.log("body" , req.body.token);
+        console.log("header", req.header("Authorization"));
+       
+        const token = req.cookies.token || req.body.token || req.header("Authorization").replace("Bearer ", "");
+        
+        if(!token || token === undefined) {
+            return res.status(401).json({
+                success:false,
+                message:'Token Missing',
+            });
+        }
+
+        //verify the token
+        try{
+            const payload = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(payload);
+            //why this ?
+            req.user = payload;
+        } catch(error) {
+            return res.status(401).json({
+                success:false,
+                message:'token is invalid',
+            });
+        }
+        next();
+    } 
+    catch(error) {
+        return res.status(401).json({
+            success:false,
+            message:'Something went wrong, while verifying the token',
+            error:error.message,
+        });
+    }
+   
+}
+
+
+exports.isStudent = (req,res,next) => {
+    try{
+            if(req.user.role !== "Student") {  // req me user ke ander role if student nhii hai to aage ka kaam higa
+                return res.status(401).json({
+                    success:false,
+                    message:'THis is a protected route for students',
+                });
+            }
+            next();
+    }
+    catch(error) {
+        return res.status(500).json({
+            success:false,
+            message:'User Role is not matching',
+        })
+    }
+}
+
+exports.isAdmin = (req,res,next) => {
+    try{
+        if(req.user.role !== "Admin") {
+            return res.status(401).json({
+                success:false,
+                message:'THis is a protected route for admin',
+            });
+        }
+        next();
+}
+catch(error) {
+    return res.status(500).json({
+        success:false,
+        message:'User Role is not matching',
+    })
+}
+}
